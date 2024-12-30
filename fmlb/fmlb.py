99748023d10afa49e55fd8153f0b50bec99e3d84
ego-lay-atman-bay <70973578+ego-lay-atman-bay@users.noreply.github.com>
2024-12-30 13:18:47 -0700

    get file metadata

--- fmlb/fmlb.py
+++ fmlb/fmlb.py
@@ -13,18 +13,47 @@ class Header:
     unkown2: dcs.U32 = 3 # ?
     num_files: dcs.U32 = 0
     data_offset: dcs.U32 = 0
+    data_size: dcs.U32 = 0
     ender: Annotated[dcs.U32, dcs.PadAfter(16)] = 0
 
+@dcs.dataclass()
+class FileMetadata:
+    type: dcs.U32
+    unknown: dcs.U32
+    unknown2: dcs.U32
+    name: Annotated[bytes, 64]
+    data_offset: dcs.U32
+    size: dcs.U32
+    index: dcs.U16
+    ender: dcs.U16
+
 class FMLB:
-    def __init__(self, file: PathOrBinaryFile):
-        self.header = None
+    def __init__(self, file: PathOrBinaryFile | None = None):
+        self.header = Header()
+        self.file_metadata: list[FileMetadata] = []
+
+        if file is not None:
+            self.read(file)
 
     def read(self, file: PathOrBinaryFile):
         with open_binary(file) as file:
             self.header = self._read_header(file)
+            self.file_metadata = self._read_file_list(file)
     
-    def _read_header(self, file: BinaryIO):
+    def _read_header(self, file: BinaryIO) -> Header:
         file.seek(0)
         return Header.from_packed(
             file.read(dcs.get_struct_size(Header))
         )
+    
+    def _read_file_list(self, file: BinaryIO):
+        metadatas = []
+        
+        for x in range(self.header.num_files):
+            metadatas.append(
+                FileMetadata.from_packed(
+                    file.read(dcs.get_struct_size(FileMetadata))
+                ),
+            )
+        
+        return metadatas
