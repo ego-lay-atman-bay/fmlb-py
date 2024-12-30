9a34b7d196eee835b623a1b5e2e3db863557057d
ego-lay-atman-bay <70973578+ego-lay-atman-bay@users.noreply.github.com>
2024-12-30 14:02:18 -0700

    start work on reading files

---
+++ fmlb/enums.py
@@ -0,0 +1,13 @@
+from enum import IntEnum
+
+class FileType(IntEnum):
+    hier = 102
+    txload = 103
+    MATERIAL = 104
+    GEOMETRY = 107
+    PRIMITIVE = 108
+    CLIP = 113
+    aniplug = 114
+    SOUND = 121
+    SOUNDBANK = 122
+    COLORSET = 123
--- fmlb/fmlb.py
+++ fmlb/fmlb.py
@@ -3,6 +3,7 @@ from typing import BinaryIO, Annotated
 import dataclasses_struct as dcs
 
 from .file_utils import PathOrBinaryFile, open_binary
+from . import enums
 
 
 @dcs.dataclass()
@@ -14,7 +15,7 @@ class Header:
     num_files: dcs.U32 = 0
     data_offset: dcs.U32 = 0
     data_size: dcs.U32 = 0
-    ender: Annotated[dcs.U32, dcs.PadAfter(16)] = 0
+    end: Annotated[bytes, dcs.PadAfter(16)] = b'END>'
 
 @dcs.dataclass()
 class FileMetadata:
@@ -25,12 +26,13 @@ class FileMetadata:
     data_offset: dcs.U32
     size: dcs.U32
     index: dcs.U16
-    ender: dcs.U16
+    end: Annotated[bytes, 4] = b'\x00\x00>>'
 
 class FMLB:
     def __init__(self, file: PathOrBinaryFile | None = None):
         self.header = Header()
         self.file_metadata: list[FileMetadata] = []
+        self.files = []
 
         if file is not None:
             self.read(file)
@@ -39,6 +41,10 @@ class FMLB:
         with open_binary(file) as file:
             self.header = self._read_header(file)
             self.file_metadata = self._read_file_list(file)
+            
+            for metadata in self.file_metadata:
+                self.files.append(self._read_file(metadata, file))
+
     
     def _read_header(self, file: BinaryIO) -> Header:
         file.seek(0)
@@ -53,7 +59,30 @@ class FMLB:
             metadatas.append(
                 FileMetadata.from_packed(
                     file.read(dcs.get_struct_size(FileMetadata))
-                ),
+                )
             )
         
         return metadatas
+    
+    def _read_file(self, file_metadata: FileMetadata, file: BinaryIO):
+        match file_metadata.type:
+            case enums.FileType.hier:
+                pass
+            case enums.FileType.txload:
+                pass
+            case enums.FileType.MATERIAL:
+                pass
+            case enums.FileType.GEOMETRY:
+                pass
+            case enums.FileType.PRIMITIVE:
+                pass
+            case enums.FileType.CLIP:
+                pass
+            case enums.FileType.aniplug:
+                pass
+            case enums.FileType.SOUND:
+                pass
+            case enums.FileType.SOUNDBANK:
+                pass
+            case enums.FileType.COLORSET:
+                pass
